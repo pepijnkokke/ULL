d3c8cd317c2f1210771d5febef04c3a3b4c00fd3
Edwin <edwin.odijk@student.uva.nl>
2016-04-06 15:22:32 +0200

    added setup of evaluation

--- segment.py
+++ segment.py
@@ -60,3 +60,31 @@ class Dist(Param):
         else:
 
             return self.corpus.countWord(word) / float(self.corpus.numWords())
+
+def evaluate(segmented_found,segmented_true,lexicon_found,lexicon_true):
+    """ Evaluate precision, recall and F0 for:
+        - Words (word boundaries placed correctly before and after the word)
+        - Lexical (lexical types found)
+        - "Potentially ambiguous boundaries, i.e. utterance boundaries not included in counts". Kind of vague, will ask on thursday
+    """
+    #Note: these are exactly the evaluations as done by Goldwater & friends
+    #words_found = #something with segmented_found
+    #words_true = #something with segmented_true
+    #P = precision(words_found,words_true)
+    #R = recall(words_found,words_true)
+    #F = f_zero(P,R)
+    LP = precision(lexicon_found,lexicon_true)
+    LR = recall(lexicon_found,lexicon_true)
+    LF = f_zero(LP/LR)
+    #BP =
+    #BR =
+    #BF = f_zero(BP,BR)
+def precision(found,true):
+    """ Number of correct / number found """
+    #ToDo
+def recall(found,true):
+    """ Number of correct / number of all correct """
+    #ToDo
+def f_zero(precision,recall):
+    """ Geometric average of precision and recall """
+    return (2*precision*recall)/(precision+recall)
