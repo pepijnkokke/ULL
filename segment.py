33ab5e768e0d22ef6757f3403c60ede1aa1c9b79
Maarten de Jonge <mdejonge1990@gmail.com>
2016-04-08 22:02:08 +0200

    implement parallelization

--- segment.py
+++ segment.py
@@ -1,11 +1,13 @@
-from sys          import argv
-from collections  import Counter
-from operator     import mul
-from random       import random
-from copy         import copy
-from cPickle      import dump, load
-from os.path      import exists
-from progress.bar import Bar
+from sys             import argv
+from collections     import Counter
+from operator        import mul
+from random          import random
+from copy            import copy
+from cPickle         import dump, load
+from os.path         import exists
+from progress.bar    import Bar
+from multiprocessing import cpu_count
+from joblib          import Parallel, delayed
 
 # w         : input word
 # l         : lexical item
@@ -146,66 +148,61 @@ def find_enclosing_boundaries(boundaries, i):
         lower -= 1
 
     upper = i + 1
-    while boundaries[upper] != 1:
+    while upper < len(boundaries) and boundaries[upper] != 1:
         upper += 1
 
     return lower, upper
 
 
-def gibbs_iteration(corpus, rho=2.0, alpha=0.5):
-    boundaries = list_or(corpus.utt_boundaries, corpus.boundaries)
-    words = get_words(corpus.text, boundaries)
-    word_counts = Counter(words)
+def gibbs_iteration(corpus, boundaries, n, word_counts, i, rho=2.0, alpha=0.5):
+    # utterance boundaries are unambiguous
+    if corpus.utt_boundaries[i] == 1:
+        return 0
 
-    bar = Bar('Evaluating boundaries', max=len(corpus.text))
-    for i, phoneme in enumerate(corpus.text):
-        # utterance boundaries are unambiguous
-        if corpus.utt_boundaries[i] == 1:
-            continue
+    phoneme = corpus.text[i]
 
-        lower, upper = find_enclosing_boundaries(boundaries, i)
-        w1 = corpus.text[lower:upper]
-        w2 = corpus.text[lower:i]
-        w3 = corpus.text[i:upper]
+    lower, upper = find_enclosing_boundaries(boundaries, i)
+    w1 = corpus.text[lower:upper]
+    w2 = corpus.text[lower:i]
+    w3 = corpus.text[i:upper]
 
-        h_utt_boundaries = corpus.utt_boundaries[:lower] + corpus.utt_boundaries[upper:]
+    h_utt_boundaries = corpus.utt_boundaries[:lower] + corpus.utt_boundaries[upper:]
 
-        if boundaries[i] == 0:
-            n_ = len(words) - 1
-        else:
-            n_ = len(words) - 2
+    if boundaries[i] == 0:
+        n_ = n - 1
+    else:
+        n_ = n - 2
 
-        n_dollar = h_utt_boundaries.count(1) - 1
-        nu = n_dollar if corpus.utt_boundaries[upper] == 1 else n_ - n_dollar
+    n_dollar = h_utt_boundaries.count(1) - 1
+    nu = n_dollar if corpus.utt_boundaries[upper] == 1 else n_ - n_dollar
 
-        if boundaries[i] == 0:
-            p_h1_factor1 = (word_counts[w1] - 1 + alpha * corpus.p0(w1)) / (n_ + alpha)
-        else:
-            p_h1_factor1 = (word_counts[w1] + alpha * corpus.p0(w1)) / (n_ + alpha)
+    if boundaries[i] == 0:
+        p_h1_factor1 = (word_counts[w1] - 1 + alpha * corpus.p0(w1)) / (n_ + alpha)
+    else:
+        p_h1_factor1 = (word_counts[w1] + alpha * corpus.p0(w1)) / (n_ + alpha)
 
-        p_h1_factor2 = (nu + rho/2) / (n_ + rho)
+    p_h1_factor2 = (nu + rho/2) / (n_ + rho)
 
-        if boundaries[i] == 0:
-            p_h2_factor1 = (word_counts[w2] + alpha * corpus.p0(w2)) / (n_ + alpha)
-            p_h2_factor3 = ((word_counts[w3] + 1 if w2 == w3 else 0 + alpha *
-                             corpus.p0(w3)) / (n_ + 1 + alpha))
-        else:
-            p_h2_factor1 = (word_counts[w2] - 1 + alpha * corpus.p0(w2)) / (n_ + alpha)
-            p_h2_factor3 = ((word_counts[w3] -1 + 1 if w2 == w3 else 0 + alpha *
-                             corpus.p0(w3)) / (n_ + 1 + alpha))
+    if boundaries[i] == 0:
+        p_h2_factor1 = (word_counts[w2] + alpha * corpus.p0(w2)) / (n_ + alpha)
+        p_h2_factor3 = ((word_counts[w3] + 1 if w2 == w3 else 0 + alpha *
+                            corpus.p0(w3)) / (n_ + 1 + alpha))
+    else:
+        p_h2_factor1 = (word_counts[w2] - 1 + alpha * corpus.p0(w2)) / (n_ + alpha)
+        p_h2_factor3 = ((word_counts[w3] -1 + 1 if w2 == w3 else 0 + alpha *
+                            corpus.p0(w3)) / (n_ + 1 + alpha))
 
-        p_h2_factor2 = (n_ - n_dollar + rho/2) / (n_ + rho)
-        p_h2_factor4 = ((nu + 1 if w2 == w3 else 0 + rho/2) / (n_ + 1 + rho))
+    p_h2_factor2 = (n_ - n_dollar + rho/2) / (n_ + rho)
+    p_h2_factor4 = ((nu + 1 if w2 == w3 else 0 + rho/2) / (n_ + 1 + rho))
 
-        p_h1 = p_h1_factor1 * p_h1_factor2
-        p_h2 = p_h2_factor1 * p_h2_factor2 * p_h2_factor3 * p_h2_factor4
+    p_h1 = p_h1_factor1 * p_h1_factor2
+    p_h2 = p_h2_factor1 * p_h2_factor2 * p_h2_factor3 * p_h2_factor4
 
-        if p_h2 > p_h1:
-            corpus.boundaries[i] = 1
+    if p_h2 > p_h1:
+        return 1
+    else:
+        return 0
 
-        bar.next()
-
-    bar.finish()
 
 def main():
     corpus = Corpus(argv[1])
@@ -217,9 +214,18 @@ def main():
 
         corpus.boundaries = boundaries
 
-    for i in range(int(argv[2])):
-        print i
-        gibbs_iteration(corpus)
+    with Parallel(n_jobs=cpu_count()) as pool:
+        for i in range(int(argv[2])):
+            print i
+
+            boundaries = list_or(corpus.utt_boundaries, corpus.boundaries)
+            words = get_words(corpus.text, boundaries)
+            n = len(words)
+            word_counts = Counter(words)
+
+
+            corpus.boundaries = pool(delayed(gibbs_iteration)(corpus, boundaries, n, word_counts, i)
+                                     for i in range(len(corpus.text)))
 
     with open(FILENAME, 'wb') as f:
         print 'Saving data to {}'.format(FILENAME)
