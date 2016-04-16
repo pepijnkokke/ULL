b460670bfd1de3314378bc05b86ec5fd679c613e
Maarten de Jonge <mdejonge1990@gmail.com>
2016-04-16 20:19:41 +0200

    clean up segment.py
    added clarifying comments and a minor change to be equivalent to the
    C++ code

--- segment.py
+++ segment.py
@@ -8,26 +8,19 @@ from os.path      import exists
 from json         import load
 from progress.bar import Bar
 
-# w         : input word
-# l         : lexical item
-# n         : number of previously generated words
-# n^l       : number of times lexical item l has occured in the n words
-# alpha     : parameter of the model
-# P#        : probability of generating a word boundary
-# phonemes  : set of phonemes
-# cache     : in principle all previously segmented utterances, but in
-#             practice we only have to store n and n^l
-# utterance : unsegmented string of phonemes
-# words     : segmented utterance
-
-# corpus : [[[Phoneme]]] (TODO: replace corpus with a dict which tracks n and n^l)
-
 
 FILENAME = 'boundaries.pickle'
 FILENAME_EVAL = 'eval.pickle'
 
 
 class Corpus:
+    """
+    Represents a training corpus. Keeps track of the text, utterance boundaries,
+    word boundaries, phoneme probabilities and word probabilities.
+
+    Boundaries are represented as bitstrings (which are represented as lists of
+    the same length as the text with the elements being 0 or 1).
+    """
     def __init__(self, path):
         utt_boundaries = [0]
         self.text = ''
@@ -62,6 +55,10 @@ class Corpus:
 
 
 def get_words(text, boundaries):
+    """
+    Returns a list of words resulting from the given string being segmented by
+    the given boundaries.
+    """
     out = []
     for phoneme, i in zip(text, boundaries):
         if i == 1:
@@ -73,6 +70,9 @@ def get_words(text, boundaries):
 
 
 def list_or(xs, ys):
+    """
+    Returns a new bitstring which is the logical or of the two given bitstrings.
+    """
     return [1 if x == 1 or y == 1 else 0 for x, y in zip(xs, ys)]
 
 
@@ -131,7 +131,7 @@ def f_zero(precision,recall):
 
 
 def find_enclosing_boundaries(boundaries, i):
-    """ Find the nearest boundaries on both sides of i """
+    """ Find the nearest boundaries on both sides of i. """
     lower = i - 1
     while boundaries[lower] != 1:
         lower -= 1
@@ -144,17 +144,26 @@ def find_enclosing_boundaries(boundaries, i):
 
 
 def gibbs_iteration(corpus, rho=2.0, alpha=0.5):
+    """
+    Perform a single Gibbs sampling iteration over the corpus.
+    Boundaries are updated in the given corpus.
+    """
+    # precompute the word counts outside of the phoneme loop for efficiency
     boundaries = list_or(corpus.utt_boundaries, corpus.boundaries)
     words = get_words(corpus.text, boundaries)
     word_counts = Counter(words)
 
+    # create a progress bar
     bar = Bar('Evaluating boundaries', max=len(corpus.text))
+
     for i, phoneme in enumerate(corpus.text):
-        # utterance boundaries are unambiguous
+        # utterance boundaries are unambiguous, so they can be skipped
         if corpus.utt_boundaries[i] == 1:
             bar.next()
             continue
 
+        # calculate the words around the potential boundary (w1 when there's no
+        # boundary, w2+w3 when there is a boundary)
         lower, upper = find_enclosing_boundaries(boundaries, i)
         w1 = corpus.text[lower:upper]
         w2 = corpus.text[lower:i]
@@ -162,10 +171,13 @@ def gibbs_iteration(corpus, rho=2.0, alpha=0.5):
 
         h_utt_boundaries = corpus.utt_boundaries[:lower] + corpus.utt_boundaries[upper:]
 
+        # since the word counts were precomputed, we have to substract 1 from
+        # the couunts of either w1 or w2 and w3 based on wether or not there was
+        # a boundary there at the time of computation
         if boundaries[i] == 0:
-            n_ = len(words) - 1
+            n_ = len(words)
         else:
-            n_ = len(words) - 2
+            n_ = len(words) - 1
 
         n_dollar = h_utt_boundaries.count(1) - 1
         nu = n_dollar if corpus.utt_boundaries[upper] == 1 else n_ - n_dollar
@@ -183,7 +195,7 @@ def gibbs_iteration(corpus, rho=2.0, alpha=0.5):
                              corpus.p0(w3)) / (n_ + 1 + alpha))
         else:
             p_h2_factor1 = (word_counts[w2] - 1 + alpha * corpus.p0(w2)) / (n_ + alpha)
-            p_h2_factor3 = ((word_counts[w3] -1 + 1 if w2 == w3 else 0 + alpha *
+            p_h2_factor3 = ((word_counts[w3] - 1 + 1 if w2 == w3 else 0 + alpha *
                              corpus.p0(w3)) / (n_ + 1 + alpha))
 
         p_h2_factor2 = (n_ - n_dollar + rho/2) / (n_ + rho)
@@ -220,9 +232,8 @@ def main():
             print 'Saving data to {}'.format(FILENAME)
             dump(corpus.boundaries, f)
 
-    #Evaluation (if true corpus is provided)
+    # Evaluation (if true corpus is provided)
     if len(argv) > 3:
-
         corpus_true = Corpus(argv[3])
         eval = evaluate(corpus, corpus_true)
 
@@ -232,5 +243,4 @@ def main():
 
 
 if __name__ == "__main__":
-    with open("model/iter_7500_3000_0.2.txt",'r') as f:
-        corpus = Corpus()
+    main()
